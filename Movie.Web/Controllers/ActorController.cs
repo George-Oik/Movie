0491f059659e698bff0e56af1d8d80fdacdb1227
Giorgos Oikonomou <geo.oikonomou.021@gmail.com>
2021-12-20 18:42:27 +0200

    Initial Plan for MVC part

---
+++ Movie.Web/Controllers/ActorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie.Web.Controllers
+{
+    public class ActorController : Controller
+    {
+        public IActionResult Create()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Movie.Web/Controllers/MovieController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        public IActionResult MoviePage()
+        {
+            return View();
+        }
+    }
+}
